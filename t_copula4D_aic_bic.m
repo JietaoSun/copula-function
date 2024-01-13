ba7db6279c284c76eb443f6408197135af439662
JietaoSun <147964478+JietaoSun@users.noreply.github.com>
2024-01-13 23:19:49 +0800

    Update t_copula4D_aic_bic.m

--- t_copula4D_aic_bic.m
+++ t_copula4D_aic_bic.m
@@ -1,24 +1,21 @@
 function [AIC, BIC] = t_copula4D_aic_bic(u, R, nu)
-% 参数：
-%   - u：边缘分布函数值构成的n行4列矩阵
-%   - R：4x4相关矩阵
-%   - nu：t Copula函数的自由度
-% 返回值：
-%   - AIC：四维t Copula函数的AIC值
-%   - BIC：四维t Copula函数的BIC值
+% Parameters:
+%   - u: n rows by 4 columns matrix of marginal distribution function values
+%   - R: 4x4 correlation matrix
+%   - nu: Degrees of freedom for the t Copula function
+% Returns:
+%   - AIC: AIC value for the 4-dimensional t Copula function
+%   - BIC: BIC value for the 4-dimensional t Copula function
 
-n = size(u, 1); % 数据点数量
+n = size(u, 1); % Number of data points
 
-% % 通过边缘分布函数值计算t分布分位数
-% z = tinv(u, nu);
+% Calculate log-likelihood value
+log_likelihood = sum(log(copulapdf('t', u, R, nu)), 'all');
 
-% 计算对数似然值
-log_likelihood = sum(log(copulapdf('t', u, R,nu)),'all');
-
-% 计算参数数量（相关矩阵的上三角元素数量 + 自由度参数）
+% Calculate the number of parameters (number of upper triangular elements in the correlation matrix + degrees of freedom parameter)
 num_params = 4 * (4 - 1) / 2 + 1;
 
-% 计算AIC和BIC值
+% Calculate AIC and BIC values
 AIC = -2 * log_likelihood + 2 * num_params;
 BIC = -2 * log_likelihood + num_params * log(n);
 end
