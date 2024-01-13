41fa2457f3186a946d7863935ef67532023fc669
JietaoSun <147964478+JietaoSun@users.noreply.github.com>
2024-01-13 22:41:48 +0800

    Update gumbel_copula_4d_pdf.m

--- gumbel_copula_4d_pdf.m
+++ gumbel_copula_4d_pdf.m
@@ -1,25 +1,25 @@
-% Gumbel Copula概率密度函数计算函数
+% Gumbel Copula probability density function calculation function
 function c_4D = gumbel_copula_4d_pdf(u1, u2, u3, u4, alpha)
-% 参数：
-%   - u1, u2, u3, u4：边缘分布函数值，长度为n的向量
-%   - alpha：已知的Gumbel Copula参数
-% 返回值：
-%   - c_4D：四维Gumbel Copula函数的PDF值
+% Parameters:
+%   - u1, u2, u3, u4: Values of the marginal distribution functions, vectors of length n
+%   - alpha: Known parameter of the Gumbel Copula
+% Return values:
+%   - c_4D: PDF values of the four-dimensional Gumbel Copula function
 
-% 二维Gumbel Copula函数CDF的表达式
+% Expression for the CDF of the two-dimensional Gumbel Copula function
 C_2D = @(u, v, alpha) exp(-((-log(u)).^alpha + (-log(v)).^alpha).^(1/alpha));
 
-% 四维Gumbel Copula函数CDF的表达式
+% Expression for the CDF of the four-dimensional Gumbel Copula function
 A = C_2D(u1, u2, alpha);
 B = C_2D(u3, u4, alpha);
 
-
-% 二维Gumbel Copula函数PDF的表达式
+% Expression for the PDF of the two-dimensional Gumbel Copula function
 c_2D = @(u, v, alpha) (alpha ./ ((-log(u)) .* (-log(v)))).*(u.*v).^(-alpha) .* (((-log(u)).^alpha + (-log(v)).^alpha).^(1/alpha - 1)) .* exp(-((-log(u)).^alpha + (-log(v)).^alpha).^(1/alpha));
 c_2D_u1_u2 = c_2D(u1, u2, alpha);
 c_2D_u3_u4 = c_2D(u3, u4, alpha);
 
-% 四维Clayton Copula函数PDF的表达式
+% Expression for the PDF of the four-dimensional Gumbel Copula function
 c_4D = c_2D(A, B, alpha) .* c_2D_u1_u2 .* c_2D_u3_u4;
 end
+
 
