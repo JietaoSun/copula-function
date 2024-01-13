ffcd26de3cf5b5d36c5d30e30deb0589a81ce362
JietaoSun <147964478+JietaoSun@users.noreply.github.com>
2024-01-13 22:32:04 +0800

    Update clayton_copula_4d_pdf.m

--- clayton_copula_4d_pdf.m
+++ clayton_copula_4d_pdf.m
@@ -1,23 +1,24 @@
 function c_4D = clayton_copula_4d_pdf(u1, u2, u3, u4, alpha)
-% 参数：
-%   - u1, u2, u3, u4：边缘分布函数值，长度为n的向量
-%   - alpha：已知的Clayton Copula参数
-% 返回值：
-%   - c_4D：四维Clayton Copula函数的PDF值
+% Parameters:
+%   - u1, u2, u3, u4: Values of the marginal distribution functions, vectors of length n
+%   - alpha: Known parameter of the Clayton Copula
+% Return values:
+%   - c_4D: PDF values of the four-dimensional Clayton Copula function
 
-% 二维Clayton Copula函数CDF的表达式
+% Expression for the CDF of the two-dimensional Clayton Copula function
 C_2D = @(u, v, alpha) (max(u.^(-alpha) + v.^(-alpha) - 1, 0)).^(-1/alpha);
 
-% 四维Clayton Copula函数CDF的表达式
+% Expression for the CDF of the four-dimensional Clayton Copula function
 A = C_2D(u1, u2, alpha);
 B = C_2D(u3, u4, alpha);
 
-% 二维Clayton Copula函数PDF的表达式
+% Expression for the PDF of the two-dimensional Clayton Copula function
 c_2D = @(u, v, alpha) ((alpha + 1) * (u .* v).^(-alpha - 1) .* (u.^(-alpha) + v.^(-alpha) - 1).^(-2 - 1/alpha)) .* (1 + (u.^(-alpha) + v.^(-alpha) - 1).^(-1/alpha));
 
 c_2D_u1_u2 = c_2D(u1, u2, alpha);
 c_2D_u3_u4 = c_2D(u3, u4, alpha);
 
-% 四维Clayton Copula函数PDF的表达式
+% Expression for the PDF of the four-dimensional Clayton Copula function
 c_4D = c_2D(A, B, alpha) .* c_2D_u1_u2 .* c_2D_u3_u4;
 end
+
