9d3f2c885d94674267a185be5c90c06e9dc30980
JietaoSun <147964478+JietaoSun@users.noreply.github.com>
2024-01-13 22:35:06 +0800

    Update estimate_gumbel_4D_alpha.m

--- estimate_gumbel_4D_alpha.m
+++ estimate_gumbel_4D_alpha.m
@@ -1,27 +1,28 @@
 function alpha_hat = estimate_gumbel_4D_alpha(u)
-% 参数：
-%   - u：边缘分布函数值构成的n行4列矩阵
-% 返回值：
-%   - alpha_hat：估计的四维Gumbel Copula参数α
+% Parameters:
+%   - u: An n-by-4 matrix of values from the marginal distribution functions
+% Return values:
+%   - alpha_hat: Estimated parameter α for the four-dimensional Gumbel Copula
 
-% 获取数据的行数和列数
+% Get the number of rows and columns of the data
 [~, d] = size(u);
 
-% 判断输入数据是否为四维
+% Check if the input data is four-dimensional
 if d ~= 4
     error('The input matrix should have 4 columns.');
 end
 
-% 定义对数似然函数
+% Define the log-likelihood function
 log_likelihood = @(alpha) -sum(log(gumbel_copula_4d_pdf(u(:,1), u(:,2), u(:,3), u(:,4), alpha)));
 
-% 设置优化选项
+% Set optimization options
 options = optimoptions('fmincon', 'Display', 'off', 'Algorithm', 'interior-point', 'TolX', 1e-6, 'TolFun', 1e-6, 'MaxIter', 1000);
 
-% 设置初始参数值和参数范围
-alpha0 = 2; % 初始参数值
-alpha_lb = 1; % 参数下界
+% Set initial parameter value and parameter range
+alpha0 = 2; % Initial parameter value
+alpha_lb = 1; % Parameter lower bound
 
-% 优化对数似然函数以估计参数α
+% Optimize the log-likelihood function to estimate parameter α
 alpha_hat = fmincon(log_likelihood, alpha0, [], [], [], [], alpha_lb, [], [], options);
 end
+
