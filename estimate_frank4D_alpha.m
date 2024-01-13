228a0b9c99649907c04968247dae732bc4902ceb
JietaoSun <147964478+JietaoSun@users.noreply.github.com>
2024-01-13 22:34:20 +0800

    Update estimate_frank4D_alpha.m

--- estimate_frank4D_alpha.m
+++ estimate_frank4D_alpha.m
@@ -1,25 +1,24 @@
 function alpha_hat = estimate_frank4D_alpha(u)
-% 参数：
-%   - u：边缘分布函数值构成的n行4列矩阵
-% 返回值：
-%   - alpha_hat：估计的四维Frank Copula参数α
+% Parameters:
+%   - u: An n-by-4 matrix of values from the marginal distribution functions
+% Return values:
+%   - alpha_hat: Estimated parameter α for the four-dimensional Frank Copula
 
-% 获取数据的行数和列数
+% Get the number of rows and columns of the data
 [~, d] = size(u);
 
-% 判断输入数据是否为四维
+% Check if the input data is four-dimensional
 if d ~= 4
     error('The input matrix should have 4 columns.');
 end
 
-% 定义对数似然函数
+% Define the log-likelihood function
 log_likelihood = @(alpha) -sum(log(frank_copula_4d_pdf(u(:,1), u(:,2), u(:,3), u(:,4), alpha)));
 
-% 优化对数似然函数以估计参数α
+% Optimize the log-likelihood function to estimate parameter α
 options = optimset('Display', 'off', 'TolX', 1e-6, 'TolFun', 1e-6, 'MaxIter', 1000);
-alpha0 = 0.1; % 初始参数值
-alpha_lb = 0; % 参数下限
-alpha_ub = Inf; % 参数上限
+alpha0 = 0.1; % Initial parameter value
+alpha_lb = 0; % Parameter lower bound
+alpha_ub = Inf; % Parameter upper bound
 alpha_hat = fmincon(log_likelihood, alpha0, [], [], [], [], alpha_lb, alpha_ub, [], options);
 end
-
